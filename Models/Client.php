7f15e17dd87c3333677ac141154600c30bc3242e
Cassandra Bazille <bazillecassandra@gmail.com>
2025-04-07 15:19:51 +0200

    new index db and client files

---
+++ Models/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+class Client {
+    public int $id;
+    public string $nom;
+    public string $prenom;
+    public string $mail;
+    public string $telephone;
+    public string $adresse;
+
+
+// public function __construct ($id, $nom, $prenom, $mail, $telephone, $adresse) {
+
+// }
+
+}
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
 require_once __DIR__ .'/lib/database.php';
+require_once __DIR__ .'/Models/Client.php';
 
+$db = new DatabaseConnection();
 
+$clients = $db->getConnection()->query('SELECT * FROM clients')->fetchAll();
+
+var_dump($clients);
 
 
 
